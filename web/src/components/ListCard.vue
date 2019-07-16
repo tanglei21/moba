825b5f42c58199949f53fb082d97d699c4885ec6
leo <tangleeei@gmail.com>
2019-07-16 08:33:29 +0800

    feat:home page

---
+++ web/src/components/ListCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <m-card :icon='icon'
+          :title="title">
+    <div class="nav jc-between">
+      <div class="nav-item"
+           :class="{active:active===index}"
+           v-for="(cate,index) in categories"
+           :key="index"
+           @click="active=index">
+        <div class="nav-link">{{cate.name}}</div>
+      </div>
+    </div>
+    <div class="pt-2">
+      <swiper>
+        <swiper-slide v-for="(cate,index) in categories"
+                      :key="index">
+          <slot name="items"
+                :category="cate"></slot>
+        </swiper-slide>
+      </swiper>
+    </div>
+  </m-card>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: { type: String, required: true },
+    title: { type: String, required: true },
+    categories: { type: Array, required: true }
+  },
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <!-- swiper -->
+    <swiper :options="swiperOption"
+            ref="mySwiper">
+      <swiper-slide>
+        <img class="w-100"
+             src="../assets/images/ee17509e4f6459e9f311599420d2bdb0.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100"
+             src="../assets/images/d06aafa609d48be5ddae167423986581.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100"
+             src="../assets/images/2275459f381ed85b93125934b4313c7e.jpeg">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right"
+           slot="pagination"></div>
+    </swiper>
+    <!-- nav -->
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3"
+             v-for="n in 10"
+             :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
+    <m-list-card icon="menu"
+                 title="新闻资讯"
+                 :categories="newsCats">
+      <template #items="{category}">
+        <router-link tag="div"
+                     to="/"
+                     v-for="(news,i) in category.newsList"
+                     :key="i"
+                     class="py-2 fs-lg d-flex">
+          <span class="text-info">[{{news.categoryName}}]</span>
+          <span class="px-2">|</span>
+          <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{news.title}}</span>
+          <span class="text-grey-1 fs-sm">{{news.date}}</span>
+        </router-link>
+      </template>
+    </m-list-card>
+    <m-card icon="menu"
+            title="精彩视频"></m-card>
+    <m-card icon="menu"
+            title="图文攻略"></m-card>
+  </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+export default {
+  filters: {
+    date (val) {
+      return dayjs(val).format("MM/DD");
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        autoplay: true,
+        pagination: {
+          el: '.pagination-home',
+        }
+      },
+      newsCats: [
+        {
+          name: '热门',
+          newsList: [{ categoryName: '公告', title: '7月16日全服不停机更新公告', date: '07/10' }]
+        },
+        {
+          name: '公告',
+          newsList: [{ categoryName: '公告', title: '7月16日全服不停机更新公告', date: '07/10' }]
+        },
+        {
+          name: '赛事',
+          newsList: [{ categoryName: '公告', title: '7月16日全服不停机更新公告', date: '07/10' }]
+        },
+        {
+          name: '活动',
+          newsList: [{ categoryName: '公告', title: '7月16日全服不停机更新公告', date: '07/10' }]
+        }
+      ],
+      heroCats: []
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
+    }
+  }
+}
+</style>
